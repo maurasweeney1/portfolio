2adb486ea5364ea3c46903a8551d92ca3c93a8ed
Maura Sweeney <123509194+maurasweeney1@users.noreply.github.com>
2025-01-13 15:17:21 -0800

    Updates

---
+++ precache-manifest.ce5c8906e477d663731d54171569ed65.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf78d74606f50d54ac21a2558c3a7b51",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "356df420ff4fff583305",
+    "url": "/portfolio/static/css/main.3dee981a.chunk.css"
+  },
+  {
+    "revision": "d20e7677726033cab44b",
+    "url": "/portfolio/static/js/2.a8a42f6f.chunk.js"
+  },
+  {
+    "revision": "47eb6f2d36abb94a27b449ca6fd11176",
+    "url": "/portfolio/static/js/2.a8a42f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6260e7c4fae40fe1fff0",
+    "url": "/portfolio/static/js/3.8761cde2.chunk.js"
+  },
+  {
+    "revision": "356df420ff4fff583305",
+    "url": "/portfolio/static/js/main.d295336c.chunk.js"
+  },
+  {
+    "revision": "2bc66efff13dd9e0496b",
+    "url": "/portfolio/static/js/runtime-main.0e19fe62.js"
+  },
+  {
+    "revision": "e9950d4cb877dd241377559130d90758",
+    "url": "/portfolio/static/media/course.e9950d4c.png"
+  },
+  {
+    "revision": "dd42bbb44518bb558aa32cdcc26cac14",
+    "url": "/portfolio/static/media/meInLisbon.dd42bbb4.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1c8aae214303e22260ac9eb90f9c15e9.js"
+  "/portfolio/precache-manifest.ce5c8906e477d663731d54171569ed65.js"
 );
 
 self.addEventListener('message', (event) => {
