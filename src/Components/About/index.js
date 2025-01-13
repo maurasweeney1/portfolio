ba7c7b5374d3f597a8ee56140b17e97bb81908f0
Maura Sweeney <123509194+maurasweeney1@users.noreply.github.com>
2025-01-13 12:49:53 -0800

    prev sections

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,9 +1,74 @@
+import React from "react";
+import meInLisbon from "../../Assets/Images/meInLisbon.jpeg";
+
 const About = () => {
+  const aboutMe = [
+    {
+      icon: "🛩️",
+      title: "TRAVEL",
+      description:
+        "I've always enjoyed exploring new places, fueling my curiosity and experiencing to different cultures, inspiring creativity and adaptability in my work and personal growth.",
+    },
+    {
+      icon: "🌍",
+      title: "DIVERSE PERSPECTIVES",
+      description:
+        "Engaging with people from different backgrounds and cultures enriches my understanding of global challenges and enhances my problem-solving by encouraging innovative approaches.",
+    },
+    {
+      icon: "🥾",
+      title: "HIKING",
+      description:
+        "Hiking helps me recharge, develop resilience, and stay focused. I love the challenges in overcoming obstacles while appreciating the journey and the views along the way.",
+    },
+    {
+      icon: "💻",
+      title: "UI/UX DESIGN",
+      description:
+        "I use creativity with technical skills to design intuitive and visually engaging user interfaces, creating meaningful digital experiences for diverse audiences.",
+    },
+  ];
   return (
-    <section className="slide-in" id="about">
-      <h2>About Me</h2>
-      <p>This is the about section.</p>
+    <section id="about" className="about-section">
+      <div className="about-container">
+        <div className="slide-in">
+          <h2 className="about-header">About Me</h2>
+          <div className="about-content">
+            <div className="about-intro">
+              <img src={meInLisbon} alt="Me in Lison" className="about-image" />
+              <div className="about-text">
+                <p>
+                  I am a junior at Gonzaga University graduating May 2026. I've
+                  had the incredible opportunity to study Computer Science,
+                  Applied Mathematics, and Entrepreneurial Leadership focusing
+                  on Software Development, Data Analytics, and Adaptive
+                  Leadership.
+                </p>
+                <p>
+                  I'm always looking for opportunities to gain new skills and
+                  put what I've learned into practice, so I am currently seeking
+                  internships to apply my skills in real world scenarios.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="about-skills slide-in">
+            <h3 className="about-header">What I Do</h3>
+            <div className="about-grid">
+              {aboutMe.map((about, index) => (
+                <div key={index} className="about-card">
+                  <div className="card-icon">{about.icon}</div>
+                  <h3 className="card-title">{about.title}</h3>
+                  <p className="card-description">{about.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
+
 export default About;
--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -2,9 +2,59 @@ import React from "react";
 
 const Skills = () => {
   return (
-    <section className="slide-in" id="skills">
+    <section id="skills" className="slide-in">
       <h2>My Skills</h2>
-      <p>This is the skills section.</p>
+      <div className="skills-container">
+        <div className="skills-top-row">
+          <div className="skills-section">
+            <h3>TOOLS</h3>
+            <ul className="tools-list">
+              <li>Command Line</li>
+              <li>Git/Github</li>
+              <li>Docker</li>
+              <li>VS Code</li>
+              <li>R Studio</li>
+              <li>MongoDB</li>
+              <li>MySQL</li>
+              <li>Jupyter Notebook</li>
+              <li>Google Colab</li>
+              <li>Tableau</li>
+            </ul>
+          </div>
+          <div className="skills-section">
+            <h3>COURSES</h3>
+            <ul className="courses-list">
+              <li>Algorithms and Abstract Data Structures</li>
+              <li>Operating Systems</li>
+              <li>Database Systems</li>
+              <li>Experimental Statistics</li>
+              <li>Data Science</li>
+              <li>Linear Algebra</li>
+              <li>Computer Security</li>
+              <li>Web Development</li>
+              <li>UI/UX Design</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="slide-in languages-skills-section">
+        <h3>LANGUAGES</h3>
+        <div className="languages">
+          <img src="java.png" alt="Java" className="java icon" />
+          <img src="c++.png" alt="C++" className="c++ icon" />
+          <img src="c.png" alt="C" className="c icon" />
+          <img src="c-sharp.png" alt="C#" className="csharp icon" />
+          <img src="python.svg" alt="Python" className="python icon" />
+          <img src="r.png" alt="R" className="r icon" />
+          <img src="sql.png" alt="SQL" className="sql icon" />
+          <img src="javascript.png" alt="Javascript" className="js icon" />
+          <img src="html.png" alt="HTML" className="html icon" />
+          <img src="css.png" alt="CSS" className="css icon" />
+          <img src="react.png" alt="React" className="react icon" />
+          <img src="node-js.png" alt="Node.js" className="nodejs icon" />
+        </div>
+      </div>
     </section>
   );
 };
